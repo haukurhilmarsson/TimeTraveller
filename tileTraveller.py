296f43bfe7657386170d38a1dd5b3f49c1b7b6fc
Haukur Breki Hilmarsson <HaukurBreki@Haukurs-MacBook-Pro.local>
2018-09-18 09:26:50 +0000

    Algorithm implementation

---
+++ tileTraveller.py
@@ -0,0 +1,6 @@
+#One big while loop to keep the player in the game
+#Player starts in tyle 1,1
+#Player enters the direction in which he wants to go
+#Player's new tyle displayed
+#Every tyle is its own variable with its own possible inputs
+#When the player gets to the tile 3,1 the while loop stops and the player wins
